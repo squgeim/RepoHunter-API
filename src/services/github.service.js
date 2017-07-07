05fc533e223b20219487ec0ea69ee450c9721998
Shreya Dahal <shreyadahal@gmail.com>
2017-07-08 00:52:13 +0545

    Add package details from package.json

--- src/services/github.service.js
+++ src/services/github.service.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import GitHub from 'github-api';
 
 if (!process.env.GITHUB_TOKEN || !process.env.GITHUB_ORGANIZATION) {
@@ -7,15 +8,50 @@ if (!process.env.GITHUB_TOKEN || !process.env.GITHUB_ORGANIZATION) {
 }
 
 const ORG = process.env.GITHUB_ORGANIZATION;
+const TOKEN = process.env.GITHUB_TOKEN;
 
 const gh = new GitHub({
-  token: process.env.GITHUB_TOKEN,
+  token: TOKEN,
 });
 
 const org = gh.getOrganization(ORG);
 
 const FILENAME = {
   npm: 'package.json',
+};
+
+const getSnippet = (query, packageJson) => {
+  const results = [];
+  const dependencies = packageJson.dependencies;
+  const devDependencies = packageJson.devDependencies;
+
+  const pacInDep = Object.keys(dependencies).filter(
+    p => p.toLowerCase().indexOf(query.toLowerCase()) !== -1
+  );
+
+  if (pacInDep.length > 0) {
+    pacInDep.forEach(pac =>
+      results.push({
+        package: pac,
+        version: dependencies[pac],
+      })
+    );
+  }
+
+  const pacInDevDep = Object.keys(devDependencies).filter(
+    p => p.toLowerCase().indexOf(query.toLowerCase()) !== -1
+  );
+
+  if (pacInDevDep.length > 0) {
+    pacInDevDep.forEach(pac =>
+      results.push({
+        package: pac,
+        version: devDependencies[pac],
+      })
+    );
+  }
+
+  return results;
 };
 
 export const getOrganizationRepos = () => {
@@ -48,16 +84,47 @@ export const searchInNpm = query => {
 
         resultRepos.add(result.repository.full_name);
 
-        return [...repos, result.repository];
+        return [
+          ...repos,
+          Object.assign({}, result.repository, {
+            fileUrl: result.html_url,
+          }),
+        ];
       })
+    )
+    .then(repos =>
+      Promise.all(
+        repos.map(repo =>
+          axios
+            .get(
+              repo.fileUrl
+                .replace('blob/', '')
+                .replace('github.com', 'raw.githubusercontent.com'),
+              {
+                headers: {
+                  Authorization: `token ${TOKEN}`,
+                },
+              }
+            )
+            .then(response => getSnippet(query, response.data))
+            .then(snippet =>
+              Object.assign({}, repo, {
+                snippet,
+              })
+            )
+        )
+      )
     )
     .then(repos =>
       repos.map(repo => ({
         name: repo.name,
-        full_name: repo.full_name,
+        fullName: repo.full_name,
         url: repo.html_url,
-        api_url: repo.url,
+        apiUrl: repo.url,
         private: repo.private,
+        description: repo.description,
+        fileUrl: repo.fileUrl,
+        snippet: repo.snippet,
       }))
     )
     .catch(err => {
